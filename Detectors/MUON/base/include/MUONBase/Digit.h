5bd209a6dc6416143e5f8b806b146338ea93f138
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2016-10-20 10:43:52 +0200

    added digit unique id encoder/decoder

--- Detectors/MUON/base/include/MUONBase/Digit.h
+++ Detectors/MUON/base/include/MUONBase/Digit.h
@@ -1,5 +1,5 @@
 //
-//  DigitSampler.h
+//  Digit.h
 //  ALICEO2
 //
 //  Created by Hugo Pereira Da Costa on Oct 18 2016
@@ -17,23 +17,64 @@ namespace AliceO2
 
   namespace MUON
   {
+
+    /// utility class to encode/decode digit unique id
+    class DigitId
+    {
+
+      public:
+
+      /// constructor
+      DigitId( uint32_t id ):
+        fId( id ),
+        fDeId( id & 0xFFF ),
+        fManuId( ( id & 0xFFF000 ) >> 12 ),
+        fManuChannel( ( id & 0x3F000000 ) >> 24 ),
+        fCathode( ( id & 0x40000000 ) >> 30 )
+      {}
+
+      /// constructor
+      DigitId( int deId, int manuChannel, int manuId, int cathode ):
+        fId( deId | ( manuId << 12 ) | ( manuChannel << 24 )  | ( cathode << 30 ) ),
+        fDeId( deId ),
+        fManuId( manuId ),
+        fManuChannel( manuChannel ),
+        fCathode( cathode )
+      {}
+
+      uint32_t fId = 0;
+      int fDeId = 0;
+      int fManuId = 0;
+      int fManuChannel = 0;
+      int fCathode = 0;
+
+      /// streamer
+      friend std::ostream& operator << ( std::ostream& out, const DigitId& digit )
+      {
+        out << "DE: " << digit.fDeId << " ManuId: " << digit.fManuId << " Channel: " << digit.fManuChannel << " Cathode: " << digit.fCathode;
+        return out;
+      }
+
+    };
+
+    /// digit storage
     struct Digit
     {
 
       public:
 
-      // unique id
+      /// unique id
       uint32_t fId = 0;
 
-      // index
+      /// index
       uint16_t fIndex = 0;
 
-      // adc
+      /// adc
       uint16_t fADC = 0;
 
       friend std::ostream& operator << ( std::ostream& out, const Digit& digit )
       {
-        out << "id: " << digit.fId << " index: " << digit.fIndex << " adc: " << digit.fADC;
+        out << DigitId( digit.fId ) << " index: " << digit.fIndex << " adc: " << digit.fADC;
         return out;
       }
 
