816dbc8929ca7dc012cd4720e1e0d0030b09916b
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2016-10-20 15:43:21 +0200

    fixed streamer

--- Detectors/MUON/base/src/PreCluster.cxx
+++ Detectors/MUON/base/src/PreCluster.cxx
@@ -1,17 +1,13 @@
 #include <MUONBase/PreCluster.h>
-#include <MUONBase/Digit.h>
 
 using namespace AliceO2::MUON;
 
 //___________________________________________________________________________________________
 std::ostream& operator << ( std::ostream& out, const PreCluster& preCluster )
 {
-  for( int i = 0; i < preCluster.fNDigits; ++i )
-  {
-    out << "PreCluster:" << std::endl;
-    out << "  Digit " << i << ": " << preCluster.fDigits[i] << std::endl;
-    return out;
-  }
+  out << "PreCluster:" << std::endl;
+  for( auto&& digit: preCluster.fDigits )
+  { out << "  Digit: " << digit << std::endl; }
 
   return out;
 
