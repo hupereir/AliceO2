f02c3508695630eb088abb3f3385218385895662
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2017-02-08 13:45:28 +0100

    used "standard" header to define main and state machine in all executables

--- Detectors/MUON/reconstruction/src/runDigitSampler.cxx
+++ Detectors/MUON/reconstruction/src/runDigitSampler.cxx
@@ -1,9 +1,6 @@
 #include "DigitSampler.h"
 
-#include <FairMQLogger.h>
-#include <FairMQProgOptions.h>
-
-#include <boost/program_options.hpp>
+#include <runFairMQDevice.h>
 
 namespace bpo = boost::program_options;
 
@@ -21,63 +18,3 @@ void addCustomOptions(bpo::options_description& options)
 //_________________________________________________________________________________________________
 FairMQDevice* getDevice(const FairMQProgOptions& /*config*/)
 { return new AliceO2::MUON::DigitSampler(); }
-
-// _________________________________________________________________________________________________
-int runStateMachine(FairMQDevice& device, FairMQProgOptions& config)
-{
-
-  device.SetConfig(config);
-  auto control = config.GetValue<std::string>("control");
-
-  // create output channel
-  auto output = config.GetValue<std::string>( "output" );
-  LOG(INFO) << "output channel: " << output;
-
-  FairMQChannel channel("push", "bind", output );
-
-  // define transport and assign channel
-  device.SetTransport("zeromq");
-  device.fChannels["data1"].push_back(channel);
-
-  device.ChangeState(FairMQDevice::INIT_DEVICE);
-  device.WaitForEndOfState(FairMQDevice::INIT_DEVICE);
-
-  device.ChangeState(FairMQDevice::INIT_TASK);
-  device.WaitForEndOfState(FairMQDevice::INIT_TASK);
-
-  device.ChangeState(FairMQDevice::RUN);
-  device.InteractiveStateLoop();
-
-  return 0;
-
-}
-
-
-//_________________________________________________________________________________________________
-int main(int argc, char** argv)
-{
-  try
-  {
-
-    boost::program_options::options_description customOptions("Custom options");
-    addCustomOptions(customOptions);
-
-    FairMQProgOptions config;
-    config.AddToCmdLineOptions(customOptions);
-    config.ParseAll(argc, argv);
-
-    std::unique_ptr<FairMQDevice> device(getDevice(config));
-    int result = runStateMachine(*device, config);
-
-    if (result > 0) return 1;
-
-  } catch (std::exception& e) {
-
-    LOG(ERROR) << "Unhandled Exception reached the top of main: " << e.what() << ", application will now exit";
-    return 1;
-
-  }
-
-  return 0;
-
-}
--- Detectors/MUON/reconstruction/src/runDigitSink.cxx
+++ Detectors/MUON/reconstruction/src/runDigitSink.cxx
@@ -1,9 +1,6 @@
 #include "DigitSink.h"
 
-#include <FairMQLogger.h>
-#include <FairMQProgOptions.h>
-
-#include <boost/program_options.hpp>
+#include <runFairMQDevice.h>
 
 namespace bpo = boost::program_options;
 
@@ -18,63 +15,3 @@ void addCustomOptions(bpo::options_description& options)
 //_________________________________________________________________________________________________
 FairMQDevice* getDevice(const FairMQProgOptions& /*config*/)
 { return new AliceO2::MUON::DigitSink(); }
-
-// _________________________________________________________________________________________________
-int runStateMachine(FairMQDevice& device, FairMQProgOptions& config)
-{
-
-  device.SetConfig(config);
-  std::string control = config.GetValue<std::string>("control");
-
-  // create output channel
-  auto source = config.GetValue<std::string>( "source" );
-  LOG(INFO) << "source channel: " << source;
-
-  FairMQChannel channel("pull", "connect", source );
-
-  // define transport and assign channel
-  device.SetTransport("zeromq");
-  device.fChannels["data1"].push_back(channel);
-
-  device.ChangeState(FairMQDevice::INIT_DEVICE);
-  device.WaitForEndOfState(FairMQDevice::INIT_DEVICE);
-
-  device.ChangeState(FairMQDevice::INIT_TASK);
-  device.WaitForEndOfState(FairMQDevice::INIT_TASK);
-
-  device.ChangeState(FairMQDevice::RUN);
-  device.InteractiveStateLoop();
-
-  return 0;
-
-}
-
-
-//_________________________________________________________________________________________________
-int main(int argc, char** argv)
-{
-  try
-  {
-
-    boost::program_options::options_description customOptions("Custom options");
-    addCustomOptions(customOptions);
-
-    FairMQProgOptions config;
-    config.AddToCmdLineOptions(customOptions);
-    config.ParseAll(argc, argv);
-
-    std::unique_ptr<FairMQDevice> device(getDevice(config));
-    int result = runStateMachine(*device, config);
-
-    if (result > 0) return 1;
-
-  } catch (std::exception& e) {
-
-    LOG(ERROR) << "Unhandled Exception reached the top of main: " << e.what() << ", application will now exit";
-    return 1;
-
-  }
-
-  return 0;
-
-}
